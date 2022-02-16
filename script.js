d8ba4c74928707383069f31bf0a4fcf269dae8fc
johnjohnn <conde2010@aol.com>
2022-02-16 18:29:23 -0500

    Making an API call to the NASA API

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+// NASA API 
+const count = 10;
+const apiKey = 'DEMO_KEY'
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = {}; 
+
+
+// Get 10 image from NASA API 
+
+async function getNasaPictures () {
+
+    try {
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+        console.log(resultsArray);
+
+    } catch (error){
+     // Catch Error
+    }
+}
+
+
+// On load 
+getNasaPictures();
